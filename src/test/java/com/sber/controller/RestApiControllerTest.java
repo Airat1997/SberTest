cd39279a74c0c3f6dba8da26bcaa5c2d1d5b8995
Wilmer Blossom <wilmerbl@ge-i5.kzn.21-school.ru>
2024-05-10 10:37:51 +0300

    refactoring test

--- src/test/java/com/sber/controller/RestApiControllerTest.java
+++ src/test/java/com/sber/controller/RestApiControllerTest.java
@@ -1,94 +1,67 @@
 package com.sber.controller;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import com.sber.repository.ProductRepository;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Test.*;
+import com.sber.model.Product;
 import com.sber.service.ProductService;
 import java.util.UUID;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.test.annotation.DirtiesContext;
+
 
 @SpringBootTest
-@ExtendWith(MockitoExtension.class)
 class RestApiControllerTest {
-
-    private MockMvc mockMvc;
-    @MockBean
-    private ProductService productRepository;
+    @Autowired
+    private ProductService productService;
     String uuidString = "0bc1978c-a219-4ccc-adfc-3157910002df";
     UUID uuid = UUID.fromString(uuidString);
-    String json = """
-            {
-                "description": "Test description",
-                "id": "0bc1978c-a219-4ccc-adfc-3157910002df",
-                "name": "Test",
-                "price": 10000.0
-            }
-            """;
-    String newJson = """
-            {
-                "description": "New Test description",
-                "id": "0bc1978c-a219-4ccc-adfc-3157910002df",
-                "name": "New Test",
-                "price": 250.0
-            }
-            """;
-
-    @BeforeEach
-    void setUp() throws Exception {
-        mockMvc = MockMvcBuilders.standaloneSetup(new RestApiController(productRepository)).build();
-    }
-
-    @AfterEach
-    void tearDown() {
-    }
-
+//    @BeforeEach
+//    void setUp() throws Exception {
+//    }
+//
+//    @AfterEach
+//    void tearDown() {
+//    }
+//
+//    ('123e4567-e89b-12d3-a456-426614174000', 'Product Name 1', 'This is a description for Product 1.', 19.99),
+//    ('123e4567-e89b-12d3-a456-426614174001', 'Product Name 2', 'This is a description for Product 2.', 29.99),
+//    ('123e4567-e89b-12d3-a456-426614174002', 'Product Name 3', 'This is a description for Product 3.', 39.99);
+    @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
     @Test
     void getProducts() throws Exception {
-        mockMvc.perform(get("/products"))
-                .andExpect(status().isOk());
-        mockMvc.perform(get("/non_correct_path"))
-                .andExpect(status().isNotFound());
+        Iterable<Product> list = productService.findAll();
+        assertEquals(3, list.spliterator().estimateSize());
     }
 
+    @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
     @Test
     void postProduct() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.post("/products")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(json))
-                .andExpect(status().isCreated());
+        Product product = new Product(uuid, "Test","Test description",123.0);
+        productService.save(product);
+        assertNotNull(productService.findById(uuid));
     }
-
+    @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
     @Test
     void putProduct() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.post("/products")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(json))
-                .andExpect(status().isCreated());
-        mockMvc.perform(MockMvcRequestBuilders.put("/products/" + uuid)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(newJson))
-                .andExpect(status().isCreated());
+        Product product = new Product(uuid, "Test","Test description",123.0);
+        productService.save(product);
+        assertNotNull(productService.findById(uuid));
+        product.setDescription("new Test");
+        productService.save(product);
+        assertEquals(productService.findById(uuid).getDescription(), "new Test");
     }
 
     @Test
     void deleteProduct() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.post("/products")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(json))
-                .andExpect(status().isCreated());
-        mockMvc.perform(MockMvcRequestBuilders.delete("/products/" + uuid)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(json))
-                .andExpect(status().isOk());
+        String uuidString = "123e4567-e89b-12d3-a456-426614174000";
+        UUID uuid2 = UUID.fromString(uuidString);
+        productService.deleteById(uuid2);
+        assertEquals(productService.findById(uuid2), null);
     }
 }
